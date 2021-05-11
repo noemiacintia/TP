3a5633eb40844b733145ae113131f1b5d860cfdd
noemiacintia <noemia.silva@ee.ufcg.edu.br>
2021-05-11 11:18:23 -0300

    Versão 4 do projeto adicionada

---
+++ Projeto/Versões/5/Cervejaria.h
@@ -0,0 +1,26 @@
+#ifndef CERVEJARIA_H
+#define CERVEJARIA_H
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+class Cervejaria{
+    protected:
+        string nome, codigo;
+        float preco;
+        
+    public:
+        Cervejaria( );
+        Cervejaria(string, string, float);
+        void setNome(string);
+        string getNome( )const;
+        void setCodigo(string);
+        string getCodigo( )const;
+        void setPreco(float);
+        float getPreco( )const;
+        void Imprime( );
+};
+#endif
